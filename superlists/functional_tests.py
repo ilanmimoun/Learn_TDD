4dd376b1c5d7836e76fef4ea1732b083c48a5778
Ilan Mimoun <ilan.mimoun@sivan-innovation.com>
2018-01-21 16:48:24 +0200

    Update test into class. (1 test failed)

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitiorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        # The software works, as quit the firefox:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Test if the home page works on Firefox:
+        self.browser.get('http://localhost:8000')
+        # Test if page title is right:
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
