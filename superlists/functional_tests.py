0d26e07a8a45c6867366d08e85cbbe839ae7394a
Ilan Mimoun <ilan.mimoun@sivan-innovation.com>
2018-01-22 11:31:58 +0200

    Add an exception to 'functional_tests.py':   - if the server is not running, show error message.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+
 import unittest
 
 
@@ -13,7 +15,15 @@ class NewVisitiorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Test if the home page works on Firefox:
-        self.browser.get('http://localhost:8000')
+        try:
+            self.browser.get('http://localhost:8000')
+        except WebDriverException:
+            error_message = "\n"
+            error_message += "Be sure the server is running.\n"
+            error_message += "In an other terminal:\n"
+            error_message += "python manage.py runserver\n"
+            self.fail(error_message)
+
         # Test if page title is right:
         self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test!')
