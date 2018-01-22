5e4e6aaf794fc206320b699ce9c5cf0bacda7a73
Ilan Mimoun <ilan.mimoun@sivan-innovation.com>
2018-01-22 15:49:06 +0200

    Test wrong - 'utils.tests':   - create file tests:     - have to create a file (superlists/utils/utils.py)     with all function usable by all apps.     - first function is 'remove_csrf':       - take a DOM as a string and remove       'csrf token' create by django.

---
+++ superlists/utils/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from utils.utils import remove_csrf
+
+
+class RemoveCSRFTest(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_without_csrf(self):
+        pass
