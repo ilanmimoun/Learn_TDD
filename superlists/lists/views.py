301b33353b977947581186bb8b689f4e2f64d034
Ilan Mimoun <ilan.mimoun@sivan-innovation.com>
2018-01-24 13:46:49 +0200

    Test wrong (1) - 'test_can_start_a_list_and_retrieve_it_later':   - 'AssertionError: '1: Buy peacock feathers' not found in ['1: Use peacock feathers to make a fly']'   - 'functional_tests.py'   - resolve previous test error:     - add the new item to the database 'Item'.

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from lists.models import Item
 
 
 def home_page(request):
+    new_item = Item()
+    new_item.text = request.POST.get('item_text', '')
+    new_item.save()
     return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', '')})
